964a838e0d00fd96154c366be567551c7ece5602
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-16 17:21:47 -0400

    Added Stdin Setup in the input file, used module to export it to play file

--- play.js
+++ play.js
@@ -1,6 +1,7 @@
-const {connect} = require("./client"); // import connect function from file client.js
-
-
+const {connect} = require("./client"); // import connect function from client.js file
+const {setupInput} = require("./input"); // import setupInput from input.js file
 console.log("Connecting ...");
+
 connect(); // call connect to establish a network.
+setupInput(); //call setupInput to establish a network.
 
