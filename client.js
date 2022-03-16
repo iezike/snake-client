709e8a8e465390820b69ddb8e601277a2dd73faa
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-16 15:32:58 -0400

    Added connection message and send client's name to server

--- client.js
+++ client.js
@@ -7,8 +7,15 @@ const connect = function (data) {
     port: 50541,// PORT number here,,
 
   });
+  //accept data input and sends it to the server
   conn.on("data", (data) => {
     console.log(data);
+  })
+  
+  //to register a "connect" handler with the printed message
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: Ike");
   })
   // interpret incoming data as text
   conn.setEncoding("utf8");
