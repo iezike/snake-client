5cc4e04f59931f166709416c0a533988512cdb59
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-16 20:24:18 -0400

    Added constants file to hold constant values

--- client.js
+++ client.js
@@ -1,35 +1,27 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function (data) {
+const connect = function(data) {
   const conn = net.createConnection({
-    host: "165.227.47.243" , // IP address here,
-    port: 50541,// PORT number here,
+    host: IP, // IP address here,
+    port: PORT,// PORT number here,
 
   });
   //accept data input and sends it to the server
   conn.on("data", (data) => {
     console.log(data);
-  })
+  });
   
   //to register a "connect" handler with the printed message
   conn.on("connect", () => {
     console.log("Successfully connected to game server"); // message to print when connected.
     conn.write("Name: Ike");   // send client's name "Ike" to serve
-    conn.write("Move: up") // move up one square (unless facing down)
-    // conn.write("Move: down") // move down one square (unless facing up)
-    // conn.write("Move: left") //move left one square (unless facing right)
-    // conn.write("Move: right") //move left one square (unless facing left)
-    // setTimeout( () => conn.write("Move: up"), 3000);
-    // setInterval( () => conn.write("Move: up"), 50);
-  })
-
-
-
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   return conn;
 };
 
-module.exports = {connect}
+module.exports = {connect};
--- input.js
+++ input.js
@@ -1,75 +1,40 @@
 const { stdout } = require("process");
 
+// Stores the active TCP connection object.
 let connection;
 
 // setup interface to handle user input from stdin
-
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  stdin.resume(); 
+  stdin.resume();
   stdin.on("data", handleUserInput);
   connection = conn;
+  setTimeout(() => connection.write("Say: EI"), 2000);
   return stdin;
 };
 
 
 
 // \u0003 maps to ctrl+c input
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   if (key === "w") {
     connection.write("Move: up");
-  } else if(key === "a") {
-    connection.write("Move: left")
-  } else if(key === "s") {
-    connection.write("Move: down")
-  } else if(key === "d") {
-    connection.write("Move: right")
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
   if (key === '\u0003') {
     process.exit();
   }
 };
 
-// // Map Movements to keyboard keys w, a , s and d
-// const userMovementKeys = function (key) {
-//   if (key === "w") {
-//     console.log("Move: up");
-//   } else if(key === "a") {
-//     console.log("Move: left")
-//   } else if(key === "s") {
-//     console.log("Move: down")
-//   } else if(key === "d") {
-//     console.log("Move: right")
-//   }
-// };
-
-
-// Stores the active TCP connection object.
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// connection.on("connect", () => {
-//   connection.write("Move: up") 
-  
-//   // move up one square (unless facing down)
-//   // conn.write("Move: down") // move down one square (unless facing up)
-//   // conn.write("Move: left") //move left one square (unless facing right)
-//   // conn.write("Move: right") //move left one square (unless facing left)
-//   // setTimeout( () => conn.write("Move: up"), 3000);
-//   // setInterval( () => conn.write("Move: up"), 50);
-// })
 module.exports = {setupInput};
